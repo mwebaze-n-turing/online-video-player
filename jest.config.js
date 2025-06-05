75ebfc3c5297752423cfed712a760a19307db5ea
mwebaze-n-turing <mwebaze.n@turing.com>
2025-06-05 11:02:10 +0300

    test configurations

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  moduleDirectories: ['node_modules', 'src'],
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest', // Use babel-jest to transform JavaScript and TypeScript files
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[tj]s?(x)',
+  ],
+};
